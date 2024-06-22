9c5360bc30799b2e3c69b342db1e0043ba38a23e
Tom Heaton <thomas.m.heaton@gmail.com>
2024-06-22 20:17:27 +0100

    start parsing commands

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # godb
 
 GoDB
+
+## examples
+
+### Creating Tables
+
+```shell
+> CREATE TABLE Dogs (Name STRING, Age INT);
+```
--- main.go
+++ main.go
@@ -7,23 +7,119 @@ import (
 	"strings"
 )
 
-func processCommand(command string) {
-	fmt.Println("Processing:", command)
+const (
+	// keywords
+	CREATE   = "CREATE"
+	DATABASE = "DATABASE"
+	TABLE    = "TABLE"
+	SELECT   = "SELECT"
+	FROM     = "FROM"
+	WHERE    = "WHERE"
+	INSERT   = "INSERT"
+	INTO     = "INTO"
+	VALUES   = "VALUES"
+	// data types
+	INT    = "INT"
+	STRING = "STRING"
+	// operators
+	EQUALS = "="
+	// others
+	OPEN_PARENTHESIS  = "("
+	CLOSE_PARENTHESIS = ")"
+	COMMA             = ","
+	SEMICOLON         = ";"
+)
 
-	parts := strings.Split(command, " ")
+type Token struct {
+	Kind  string
+	Value string
+}
 
-	if len(parts) == 0 {
-		fmt.Println("Invalid command")
-		return
+func parseCommand(input string) {
+	fmt.Println("Parsing:", input)
+
+	lexems := make([]string, 0)
+	lexem := ""
+
+	for _, r := range input {
+		if r == ' ' {
+			fmt.Println("Lexem:", lexem)
+			lexems = append(lexems, lexem)
+			lexem = ""
+
+			continue
+		}
+
+		if r == '(' || r == ')' || r == ',' {
+			fmt.Println("Lexem:", lexem)
+			lexems = append(lexems, lexem)
+			lexem = ""
+
+			lexems = append(lexems, string(r))
+
+			continue
+		}
+
+		if r == ';' {
+			fmt.Println("Lexem:", lexem)
+			lexems = append(lexems, lexem)
+			lexem = ""
+
+			break
+		}
+
+		lexem += string(r)
 	}
 
-	switch strings.ToLower(parts[0]) {
-	case "create":
-		switch strings.ToLower(parts[1]) {
-		case "table":
-			tableName := parts[2]
-			fmt.Println("Creating table:", tableName)
+	fmt.Println("Lexems:", lexems)
+	for _, lexem := range lexems {
+		fmt.Printf("Lexem: '%s'\n", lexem)
+	}
+
+	tokens := make([]Token, 0)
+
+	for _, lexem := range lexems {
+		switch lexem {
+		case CREATE:
+			tokens = append(tokens, Token{Kind: "KEYWORD", Value: lexem})
+		case DATABASE:
+			tokens = append(tokens, Token{Kind: "KEYWORD", Value: lexem})
+		case TABLE:
+			tokens = append(tokens, Token{Kind: "KEYWORD", Value: lexem})
+		case SELECT:
+			tokens = append(tokens, Token{Kind: "KEYWORD", Value: lexem})
+		case FROM:
+			tokens = append(tokens, Token{Kind: "KEYWORD", Value: lexem})
+		case WHERE:
+			tokens = append(tokens, Token{Kind: "KEYWORD", Value: lexem})
+		case INSERT:
+			tokens = append(tokens, Token{Kind: "KEYWORD", Value: lexem})
+		case INTO:
+			tokens = append(tokens, Token{Kind: "KEYWORD", Value: lexem})
+		case VALUES:
+			tokens = append(tokens, Token{Kind: "KEYWORD", Value: lexem})
+		case INT:
+			tokens = append(tokens, Token{Kind: "DATA_TYPE", Value: lexem})
+		case STRING:
+			tokens = append(tokens, Token{Kind: "DATA_TYPE", Value: lexem})
+		case EQUALS:
+			tokens = append(tokens, Token{Kind: "OPERATOR", Value: lexem})
+		case OPEN_PARENTHESIS:
+			tokens = append(tokens, Token{Kind: "OTHER", Value: lexem})
+		case CLOSE_PARENTHESIS:
+			tokens = append(tokens, Token{Kind: "OTHER", Value: lexem})
+		case COMMA:
+			tokens = append(tokens, Token{Kind: "OTHER", Value: lexem})
+		case SEMICOLON:
+			tokens = append(tokens, Token{Kind: "OTHER", Value: lexem})
+		default:
+			tokens = append(tokens, Token{Kind: "OTHER", Value: lexem})
 		}
+	}
+
+	fmt.Println("Tokens:", tokens)
+	for _, token := range tokens {
+		fmt.Println("Token:", token)
 	}
 }
 
@@ -35,7 +131,7 @@ func main() {
 	for {
 		fmt.Print("> ")
 
-		input, err := reader.ReadString('\n')
+		input, err := reader.ReadString(';')
 		if err != nil {
 			fmt.Println("Error:", err)
 			continue
@@ -43,12 +139,12 @@ func main() {
 
 		input = strings.TrimSpace(input)
 
-		if input == "quit" || input == "QUIT" {
+		if input == "QUIT" {
 			fmt.Println("Quiting GoDB...")
 			return
 		}
 
 		fmt.Println("Input:", input)
-		processCommand(input)
+		parseCommand(input)
 	}
 }
